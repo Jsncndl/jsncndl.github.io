3b7b277094b5aa9fd9a3569be136eef4b0c3f00a
Jsncndl <jsncndl1@gmail.com>
2024-07-30 17:17:04 +0200

    add next-themes and theme logic and button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        {/* <ThemeProvider attribute="class"> */}{children}{/* </ThemeProvider> */}
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
     </html>
   );
