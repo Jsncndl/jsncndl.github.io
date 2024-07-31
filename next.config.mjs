edcc6730586ecaecbea0dd996c37bb3759fe42bb
Jsncndl <jsncndl1@gmail.com>
2024-07-31 10:55:37 +0200

    Edit for GitHub Pages deployment on jsncndl.github.io

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/jcdev",
-  assetPrefix: "/jcdev/",
+  basePath: "/",
+  assetPrefix: "/",
   images: {
     unoptimized: true,
   },
