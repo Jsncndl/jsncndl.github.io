7a13d23cb6b34cb69b6cff9dbf3a41c864194994
Jsncndl <jsncndl1@gmail.com>
2025-03-04 22:55:32 +0100

    edit section services

--- src/app/_components/NewServices.tsx
+++ src/app/_components/NewServices.tsx
@@ -1,36 +1,12 @@
-import Image from "next/image";
+import NewServices_Card from "./NewServices_Card";
 
 const NewServices = () => {
   return (
     <section>
-      <h2>Mes services</h2>
-      <div className="card">
-        <div className="card-image">
-          <Image
-            src="/undraw_progressive-app_9517.svg"
-            alt="image"
-            width={300}
-            height={300}
-          />
-        </div>
-        <div className="card-content">
-          <h3 className="card-content-title">Site vitrine</h3>
-          <div className="card-content-text">
-            <ul className="card-content-text-list">
-              <li className="card-content-text-list-item">
-                Présentation de votre entreprise
-              </li>
-              <li className="card-content-text-list-item">
-                Portfolio de vos réalisations
-              </li>
-              <li className="card-content-text-list-item">
-                Formulaire de contact
-              </li>
-              <li className="card-content-text-list-item">Réseaux sociaux</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <h2 className="text-3xl">Mes services</h2>
+      {[...Array(3)].map((_, index) => (
+        <NewServices_Card key={index} index={index} />
+      ))}
     </section>
   );
 };
